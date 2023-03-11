2e39cc91be7d08517f97dcf922e3326c91f2eff3
Matt Payne <Payne@MattPayne.org>
2023-03-11 12:05:14 -0600

    It runs -- need to start state machine etc in a command line runner
    Sorta following https://github.com/spring-projects/spring-statemachine/blob/main/spring-statemachine-samples/persist/src/main/java/demo/persist/StateMachineCommands.java

--- src/main/java/org/mattpayne/learning/sm3p1/Sm3p1Application.java
+++ src/main/java/org/mattpayne/learning/sm3p1/Sm3p1Application.java
@@ -1,11 +1,104 @@
 package org.mattpayne.learning.sm3p1;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.EnableStateMachine;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
+import org.springframework.statemachine.recipes.persist.PersistStateMachineHandler;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
 public class Sm3p1Application {
+
+
+
+
+
+    //tag::snippetA[]
+    @Configuration
+    @EnableStateMachine
+    static class StateMachineConfig
+            extends StateMachineConfigurerAdapter<String, String> {
+
+        @Override
+        public void configure(StateMachineStateConfigurer<String, String> states)
+                throws Exception {
+            states
+                    .withStates()
+                    .initial("PLACED")
+                    .state("PROCESSING")
+                    .state("SENT")
+                    .state("DELIVERED");
+        }
+
+        @Override
+        public void configure(StateMachineTransitionConfigurer<String, String> transitions)
+                throws Exception {
+            transitions
+                    .withExternal()
+                    .source("PLACED").target("PROCESSING")
+                    .event("PROCESS")
+                    .and()
+                    .withExternal()
+                    .source("PROCESSING").target("SENT")
+                    .event("SEND")
+                    .and()
+                    .withExternal()
+                    .source("SENT").target("DELIVERED")
+                    .event("DELIVER");
+        }
+
+    }
+//end::snippetA[]
+
+    //tag::snippetB[]
+    @Configuration
+    static class PersistHandlerConfig {
+
+        @Autowired
+        private StateMachine<String, String> stateMachine;
+
+        @Bean
+        public Persist persist() {
+            return new Persist(persistStateMachineHandler());
+        }
+
+        @Bean
+        public PersistStateMachineHandler persistStateMachineHandler() {
+            return new PersistStateMachineHandler(stateMachine);
+        }
+
+    }
+//end::snippetB[]
+
+    //tag::snippetC[]
+    public static class Order {
+        int id;
+        String state;
+
+        public Order(int id, String state) {
+            this.id = id;
+            this.state = state;
+        }
+
+        @Override
+        public String toString() {
+            return "Order [id=" + id + ", state=" + state + "]";
+        }
+
+    }
+//end::snippetC[]
+
+//     public static void main(String[] args) throws Exception {
+//         Bootstrap.main(args);
+//    }
 
     public static void main(final String[] args) {
         SpringApplication.run(Sm3p1Application.class, args);
